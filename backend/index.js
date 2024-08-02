829f96fd76f5a4f68a59e16e8e86b332c2c46db4
Moustafa <moustafaadel097@gmail.com>
2024-08-02 20:19:17 +0300

    exposed headers

--- backend/index.js
+++ backend/index.js
@@ -18,22 +18,22 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, parameterLimit: 50000 }));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.use(
   cors({
     origin: ["https://questhub-ten.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Set-Cookie"],
     optionSuccessStatus: 200,
     // for cookies::
     credentials: true,
   })
 );
-
-app.use(cookieParser());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.use("/user", userRoutes);
 app.get("/", (req, res) => {
