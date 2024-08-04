569f9379f642986f305824ad4b272e42757eb43d
Moustafa <moustafaadel097@gmail.com>
2024-08-04 10:37:02 +0300

    test

--- backend/utils/generateUserToken.js
+++ backend/utils/generateUserToken.js
@@ -7,9 +7,8 @@ module.exports.GenerateUserToken = (res, ID, next) => {
   res.cookie(process.env.USER_TOKEN, token, {
     withCredentials: true,
     maxAge: maxAge,
-    secure: true,
+    // secure: true,
     httpOnly: true,
-    domain: "questhub-ek4w.vercel.app",
-    sameSite: "strict",
+    sameSite: "none",
   });
 };
