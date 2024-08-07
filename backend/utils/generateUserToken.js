21689ad4cd1d87c1cf63978c8733fcc6c8483036
Moustafa-97 <139653986+Moustafa-97@users.noreply.github.com>
2024-08-07 11:48:36 +0300

    Update generateUserToken.js

--- backend/utils/generateUserToken.js
+++ backend/utils/generateUserToken.js
@@ -5,9 +5,9 @@ module.exports.GenerateUserToken = (res, ID, next) => {
   const token = jwt.sign({ ID }, process.env.SECRET_KEY, { expiresIn: maxAge });
 
   res.cookie(process.env.USER_TOKEN, token, {
-    withCredentials: true,
+   // withCredentials: true,
     maxAge: maxAge,
-    // httpOnly: true,
+    httpOnly: true,
     secure: true,
     sameSite: "none",
   });
