1d5c3b76ca0e994f0daeb05329104d5379877950
Moustafa <moustafaadel097@gmail.com>
2024-08-08 00:38:03 +0300

    4

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv").config();
-const server = require("http").createServer();
+// const server = require("http").createServer();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
